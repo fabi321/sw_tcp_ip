1d272a26db65f50deb03eb6a77019e172ad4899c
Fabian Wunsch <fabian@uriah.heep.sax.de>
2023-04-01 17:26:00 +0200

    packet sniffer now sends http telemetry

--- packet_sniffer.lua
+++ packet_sniffer.lua
@@ -1,6 +1,11 @@
 require('packeting')
+label = property.getText('label')
+
+---@type number
+tick = 0
 
 function onTick()
+    tick = tick + 1
     packet = to_packet(1)
     if packet.ttl > 0 then
         output.setBool(1, true)
@@ -12,5 +17,18 @@ function onTick()
         sn(6, packet.ack_nmb)
         sn(7, packet.proto)
         sn(8, packet.ttl + 1)
+        async.httpGet(8080, string.format(
+                "/%s?tick=%i&src_addr=%s&dest_addr=%s&src_port=%i&dest_port=%i&seq_nmb=%i&ack_nmb=%i&proto=%i&ttl=%i",
+                label,
+                tick,
+                packet.src_addr,
+                packet.dest_addr,
+                packet.src_port,
+                packet.dest_port,
+                packet.seq_nmb,
+                packet.ack_nmb,
+                packet.proto,
+                packet.ttl + 1
+        ))
     end
 end
