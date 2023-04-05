a4bc73d7eb1d1f9e24cd0bb7c7073db4d38b42bc
Fabian Wunsch <fabian@uriah.heep.sax.de>
2023-04-05 23:22:02 +0200

    New Wi-Fi with broadcast support

--- src/wifi/ap.lua
+++ src/wifi/ap.lua
@@ -1,6 +1,8 @@
 i=input;o=output
 gn=i.getNumber;gb=i.getBool;sn=o.setNumber;sb=o.setBool
 
+require('packeting')
+require('packet_queue')
 require('wifi/frequency_core')
 
 ---@param f number
@@ -19,9 +21,16 @@ end
 tick = 0
 ---@type number
 secret = -1
+is_wifi = true
+largest_interface_id = 2
 
 function onTick()
-    rand_source = gn(10)
+    is_wifi = true
+    largest_interface_id = 2
+    for i=1,32 do
+        sn(i, 0)
+    end
+    rand_source = gn(17)
     if secret == -1 then
         if rand_source ~= 0 then
             xorshift:seed(f_to_i(rand_source))
@@ -30,8 +39,24 @@ function onTick()
         end
     else
         tick = tick + 1
-        sn(10, tick)
-        sn(11, secret)
+        local wifi_packet = to_packet(1)
+        if wifi_packet.ttl > 0 then
+            receive_packet(wifi_packet, 1)
+        end
+        local router_packet = to_packet(9)
+        if router_packet.ttl > 0 then
+            receive_packet(router_packet, 2)
+        end
+        wifi_packet = send_packet(1)
+        if wifi_packet ~= nil then
+            to_channels(wifi_packet, 1)
+        end
+        router_packet = send_packet(2)
+        if router_packet ~= nil then
+            to_channels(router_packet, 9)
+        end
+        sn(17, tick)
+        sn(18, secret)
         freq_1, freq_2 = get_frequencies(tick, secret)
         sn(20, freq_1)
         sn(21, freq_2)
