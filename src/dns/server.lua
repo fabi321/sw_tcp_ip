6ecbb5d7687ced132deee0c3a6f7c68ca9c95c23
Fabian Wunsch <fabian@uriah.heep.sax.de>
2023-04-07 10:01:20 +0200

    fix dns server

--- src/dns/server.lua
+++ src/dns/server.lua
@@ -1,12 +1,21 @@
-require('dhcp_client')
-
 ---@type table<string, string>
 dns_server_entries = {}
 
 ---@param packet Packet
+---@return Packet | nil
 function respond_to_dns(packet)
     if packet.src_port == 1 then
-        send_own_packet(packet.src_addr, 4, dns_server_entries[packet.data], 1)
+        return --[[---@type Packet]] {
+            src_addr = "ffff",
+            dest_addr = packet.src_addr,
+            src_port = 1,
+            dest_port = 0,
+            seq_nmb = 0,
+            ack_nmb = 0,
+            proto = 4,
+            ttl = 255,
+            data = dns_server_entries[packet.data]
+        }
     elseif packet.src_port == 3 then
         dns_server_entries[packet.data] = packet.src_addr
     elseif packet.src_port == 4 then
