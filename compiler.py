27bd642d000346a398c11bda2b3794e930ec99b3
Fabian Wunsch <fabian@uriah.heep.sax.de>
2023-04-10 23:46:59 +0200

    compiler has logging and argparse now

--- compiler.py
+++ compiler.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import os
 from subprocess import Popen, PIPE
 from dotenv import load_dotenv
@@ -9,6 +11,8 @@ import shutil
 from pathlib import Path
 from datetime import datetime, timedelta
 from itertools import chain
+import argparse
+import logging
 
 
 REQUIRE_REGEX: re.Pattern = re.compile(r'''require\(["']([a-zA-Z0-9_/-]+)["']\)''')
@@ -96,6 +100,8 @@ def merge_snippets(snippets: dict[str, Snippet], name: str) -> str:
 			merged_content = re.sub(f'require\\(["\']{dep_name}["\']\\)', replace_with, merged_content)
 		merged_content += snippet.content
 
+	logging.debug(f'mro for snippet {name}: {mro}')
+
 	# replace remaining dependencies with empty string
 	for dep_name in merged_deps:
 		merged_content = re.sub(f'require\\(["\']{dep_name}["\']\\)', '', merged_content)
@@ -104,9 +110,12 @@ def merge_snippets(snippets: dict[str, Snippet], name: str) -> str:
 
 
 def compile_file(snippets: dict[str, Snippet], name: str) -> str:
+	logging.debug(f'merging snippets of {name}')
 	text: str = merge_snippets(snippets, name)
+	logging.debug(f'minifying {name}')
 	text = minify(text)
-	assert len(text) <= 4096, f'script {name} is too long'
+	if len(text) > 4096:
+		logging.warning(f'script {name} is too long')
 	text = escape(text)
 	return text
 
@@ -115,25 +124,29 @@ def path_to_short_name(path: Path) -> str:
 	return '/'.join(chain(path.parts[1:-1], (path.stem,)))
 
 
-def compile() -> dict[str, str]:
+def compile_lua_files() -> dict[str, str]:
 	snippets: dict[str, Snippet] = {}
 	for file in Path('src').glob("**/*.lua"):
 		if not file.is_file():
 			# Ignores directories for now
 			continue
 		with file.open() as f:
+			logging.debug(f'reading snippet {file}')
 			snippets[path_to_short_name(file)] = Snippet(f.read())
 	return {name: compile_file(snippets, name) for name in snippets.keys()}
 
 
 def process_microcontroller(mc_hull: Path, compiled: dict[str, str]):
+	logging.debug(f'processing {mc_hull}')
 	with mc_hull.open() as f:
 		hull: str = f.read()
 	while match := TEMPLATE_REGEX.search(hull):
+		logging.debug(f'processing match {match.group()}')
 		required_snippet: Optional[str] = compiled.get(match.group(1))
 		if not required_snippet:
 			raise ModuleNotFoundError(f'Could not find snippet {match.group(1)}')
 		hull = hull[:match.start()] + required_snippet + hull[match.end():]
+	logging.debug(f'writing {mc_hull}')
 	with (Path('generated_microcontrollers') / f'{mc_hull.stem}.xml').open('w') as f:
 		f.write(hull)
 
@@ -142,13 +155,16 @@ def proces_properties() -> dict[str, str]:
 	properties: dict[str, str] = {}
 	for file in Path('properties').glob('**/*.property'):
 		with file.open() as f:
+			logging.debug(f'reading property {file}')
 			content: str = f.read()
-			assert len(content) <= 4096, f'property {file.stem} si too long'
+			if len(content) > 4096:
+				logging.warning(f'property {file.stem} is too long')
 			properties[path_to_short_name(file)] = content
 	return properties
 
 
-def install():
+def install_globally():
+	logging.info('installing microcontrollers to game directory')
 	copy_dir: Path
 	if sys.platform == 'win32':
 		copy_dir = Path(os.path.expandvars('%APPDATA%'))
@@ -156,21 +172,27 @@ def install():
 		copy_dir = Path('~/.steam/steam/steamapps/compatdata/573090/pfx/drive_c/users/steamuser/AppData/Roaming').expanduser()
 	else:
 		raise NotImplementedError('Apple is not supported atm')
+	logging.debug(f'using base_directory {copy_dir}')
 	copy_dir = copy_dir / 'Stormworks' / 'data' / 'microprocessors'
+	logging.debug(f'using microprocessor directory {copy_dir}')
 	newest = datetime.now()
 	for mc in sorted(Path('generated_microcontrollers').glob('*.xml')):
+		logging.debug(f'copying {mc}')
 		shutil.copy(mc, copy_dir)
 		dest_file: Path = copy_dir / mc.name
 		assert dest_file.is_file()
 		os.utime(dest_file, (newest.timestamp(), newest.timestamp()))
 		newest -= timedelta(minutes=1)
 	for thumb in Path('microcontroller_thumbnails').glob('*.png'):
+		logging.debug(f'copying {thumb}')
 		shutil.copy(thumb, copy_dir)
 
 
 def install_in_file(file: Path):
+	logging.info(f'installing into file {file}')
 	mcs: dict[str, str] = {}
 	for mc in Path('generated_microcontrollers').glob('*.xml'):
+		logging.debug(f'reading microcontroller {mc}')
 		with mc.open() as f:
 			file_content = f.read()
 		name = re.search(r'<microprocessor name="([^"]*)"', file_content).group(1)
@@ -178,36 +200,73 @@ def install_in_file(file: Path):
 		file_content = file_content.replace('<?xml version="1.0" encoding="UTF-8"?>', '')
 		file_content = file_content.replace('\n', ' ').replace('\t', '')
 		mcs[name] = file_content
+	logging.debug('reading vehicle file')
 	with file.open() as f:
 		vehicle = f.read()
 	start: int = 0
 	while match := MC_REGEX.search(vehicle, start):
+		logging.debug(f'found microcontroller {match.group(1)}')
 		start = match.start() + 10
 		name = match.group(1)
 		if name in mcs:
+			logging.debug('replacing microcontroller with updated variant')
 			vehicle = vehicle[:match.start()] + mcs[name] + vehicle[match.end():]
+	logging.debug('writing vehicle file')
 	with file.open('w') as f:
 		f.write(vehicle)
 
 
-def main():
-	load_dotenv()
-	do_install: bool = len(sys.argv) >= 2 and sys.argv[1] == 'install'
-	compiled: dict[str, str] = compile()
+def compile_all() -> dict[str, str]:
+	logging.info('compiling lua files')
+	compiled: dict[str, str] = compile_lua_files()
 	compiled['trusted_mode'] = (getenv('TRUSTED_MODE') or 'true').lower()
-	compiled.update(proces_properties())
 	assert compiled['trusted_mode'] in ('true', 'false'), 'Only true or false are possible settings for TRUSTED_MODE'
+	logging.info('processing properties')
+	compiled.update(proces_properties())
+	return compiled
+
+
+def install_locally(compiled: dict[str, str]):
+	logging.info(f'writing generated microcontrollers')
 	generated_microcontrollers_dir: Path = Path('generated_microcontrollers')
 	if not generated_microcontrollers_dir.is_dir():
+		logging.debug('creating generated_microcontrollers directory')
 		generated_microcontrollers_dir.mkdir()
 	for mc_hull in Path('microcontroller_hulls').iterdir():
 		if not mc_hull.is_file():
 			continue
 		process_microcontroller(mc_hull, compiled)
-	if do_install:
-		install()
-		if len(sys.argv) >= 3:
-			install_in_file(Path(sys.argv[2]).expanduser().resolve())
+
+
+def main():
+	parser: argparse.ArgumentParser = argparse.ArgumentParser(
+		description='Compiles many artifacts of different kinds into stormworks microcontrollers'
+	)
+	parser.add_argument(
+		'-d', '--debug',
+		help="Print lots of debugging statements",
+		action="store_const", dest="loglevel", const=logging.DEBUG,
+		default=logging.WARNING,
+	)
+	parser.add_argument(
+		'-v', '--verbose',
+		help="Be verbose",
+		action="store_const", dest="loglevel", const=logging.INFO,
+	)
+	subparsers = parser.add_subparsers(dest='command', required=True)
+	build_parser: argparse.ArgumentParser = subparsers.add_parser('build', help='build microcontrollers')
+	install_parser: argparse.ArgumentParser = subparsers.add_parser('install', help='build and install microcontrollers')
+	install_parser.add_argument('target', nargs='?', type=Path, help='update all microcontrollers in a saved vehicle')
+	args = parser.parse_args()
+	logging.basicConfig(format='%(asctime)s|%(levelname)s| %(message)s', datefmt='%Y-%m-%dT%H:%M:%S%z', level=args.loglevel)
+	load_dotenv()
+	if args.command in ('compile', 'install'):
+		compiled: dict[str, str] = compile_all()
+		install_locally(compiled)
+	if args.command == 'install':
+		install_globally()
+		if args.target:
+			install_in_file(args.target.expanduser().resolve())
 
 
 if __name__ == "__main__":
