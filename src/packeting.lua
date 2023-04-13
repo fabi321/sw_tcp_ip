5690365008913d3c57aab0479d325e5977657f97
Fabian Wunsch <fabian@uriah.heep.sax.de>
2023-04-13 16:23:15 +0200

    possibility to encrypt packages in transmit added

--- src/packeting.lua
+++ src/packeting.lua
@@ -12,48 +12,71 @@ require('util')
 ---@field data string
 
 ---@param nmb number
----@param start number
----@param length number
----@return number
-function byte_to_int(nmb, start, length)
-    ---@type number, number
-    local int, mask = ("I4"):unpack((("f"):pack(nmb))), 2 ^ (length * 8) - 1
-    mask = mask << (start - 1) * 8
-    return (int & mask) >> (start - 1) * 8
-end
-
----@param nmb number
 ---@param length number
 ---@return string
 function int_to_byte(nmb, length)
     return ("I%i"):format(length):pack(nmb)
 end
 
+I4_FORMATTER = "I4"
+F_FORMATTER = "f"
+
 ---@param start_channel number
+---@param encrypted boolean
+---@return number[]
+function get_numbers(start_channel, encrypted)
+    ---@type number[]
+    local numbers = {}
+    for i=1, 8 do
+        numbers[i] = (I4_FORMATTER:unpack((F_FORMATTER:pack(gn(start_channel + i - 1)))))
+    end
+    if encrypted then
+        for i=1, 8, 2 do
+            numbers[i], numbers[i + 1] = blowfish_decrypt(numbers[i], numbers[i + 1])
+        end
+    end
+    return numbers
+end
+
+---@param numbers number[]
+---@param start_channel number
+---@param encrypted boolean
+function set_numbers(numbers, start_channel, encrypted)
+    if encrypted then
+        for i=1, 8, 2 do
+            numbers[i], numbers[i + 1] = blowfish_encrypt(numbers[i], numbers[i + 1])
+        end
+    end
+    for i=1,8 do
+        sn(start_channel + i - 1, (F_FORMATTER:unpack((I4_FORMATTER:pack(numbers[i])))))
+    end
+end
+
+---@overload fun(start_channel: number): Packet
+---@param start_channel number
+---@param encrypted boolean | nil
 ---@return Packet
-function to_packet(start_channel)
-    local data = ("fffff"):pack(
-                gn(start_channel + 3),
-                gn(start_channel + 4),
-                gn(start_channel + 5),
-                gn(start_channel + 6),
-                gn(start_channel + 7))
+function to_packet(start_channel, encrypted)
+    local numbers = get_numbers(start_channel, encrypted or false)
+    local data = ("I4I4I4I4I4"):pack(numbers[4], numbers[5], numbers[6], numbers[7], numbers[8])
     return --[[---@type Packet]] {
-        src_addr = ADDRESS_FORMAT:format(byte_to_int(gn(start_channel), 1, 2)),
-        dest_addr = ADDRESS_FORMAT:format(byte_to_int(gn(start_channel), 3, 2)),
-        src_port = byte_to_int(gn(start_channel + 1), 1, 1),
-        dest_port = byte_to_int(gn(start_channel + 1), 2, 1),
-        seq_nmb = byte_to_int(gn(start_channel + 1), 3, 1),
-        ack_nmb = byte_to_int(gn(start_channel + 1), 4, 1),
-        proto = byte_to_int(gn(start_channel + 2), 1, 1),
-        ttl = byte_to_int(gn(start_channel + 2), 2, 1) - 1,
-        data = data:sub(1, byte_to_int(gn(start_channel + 2), 3, 1))
+        src_addr = ADDRESS_FORMAT:format(numbers[1] & TWO_BYTE_MASK),
+        dest_addr = ADDRESS_FORMAT:format(numbers[1] >> 16 & TWO_BYTE_MASK),
+        src_port = numbers[2] & ONE_BYTE_MASK,
+        dest_port = numbers[2] >> 8 & ONE_BYTE_MASK,
+        seq_nmb = numbers[2] >> 16 & ONE_BYTE_MASK,
+        ack_nmb = numbers[2] >> 24 & ONE_BYTE_MASK,
+        proto = numbers[3] & ONE_BYTE_MASK,
+        ttl = (numbers[3] >> 8 & ONE_BYTE_MASK) - 1,
+        data = data:sub(1, numbers[3] >> 16 & ONE_BYTE_MASK)
     }
 end
 
+---@overload fun(packet: Packet, start_channel: number)
 ---@param packet Packet
 ---@param start_channel number
-function to_channels(packet, start_channel)
+---@param encrypted boolean | nil
+function to_channels(packet, start_channel, encrypted)
     local text = (
             int_to_byte(tonumber(packet.src_addr, 16), 2)
             .. int_to_byte(tonumber(packet.dest_addr, 16), 2)
@@ -67,7 +90,10 @@ function to_channels(packet, start_channel)
             .. "\0"
             .. (packet.data .. "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0"):sub(1, 20)
     )
-    for i=0,7 do
-        sn(start_channel + i, (("f"):unpack(text:sub(i * 4 + 1, i * 4 + 4))))
+    ---@type number[]
+    local numbers = {}
+    for i=1,8 do
+        numbers[i] = (I4_FORMATTER:unpack(text:sub(i * 4 - 3, i * 4)))
     end
+    set_numbers(numbers, start_channel, encrypted or false)
 end
--- src/util.lua
+++ src/util.lua
@@ -4,3 +4,6 @@ gn=i.getNumber;gb=i.getBool;sn=o.setNumber;sb=o.setBool
 broadcast_address = "ffff"
 TIMEOUT = 120
 ADDRESS_FORMAT = "%04x"
+
+ONE_BYTE_MASK = 0xff
+TWO_BYTE_MASK = 0xffff
